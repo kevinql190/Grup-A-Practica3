54101fc6f4f3ba0a3b39481a8bcffbc76ebf3817
lauraxiaoperis <160018903+lauraxiaoperis@users.noreply.github.com>
2024-05-26 20:09:37 +0200

    dialeg 1 OK

--- Assets/-- ASSETS PROGRAMACIÓ --/Dialogue/Scripts - Dialogue/Actions.cs
+++ Assets/-- ASSETS PROGRAMACIÓ --/Dialogue/Scripts - Dialogue/Actions.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Actions : MonoBehaviour
 {
-    public void Move() 
-    { 
-        
+    private NavMeshAgent navMeshAgent;
+    private NavMeshObstacle navMeshObstacle;
+    public Transform destinationObject;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshObstacle = GetComponent<NavMeshObstacle>();
+    }
+
+    public void Move()
+    {
+        StartCoroutine(MoveAfterDelay(1.0f));
+    }
+    private IEnumerator MoveAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (navMeshObstacle != null)
+        {
+            navMeshObstacle.enabled = false;
+        }
+
+        if (navMeshAgent != null)
+        {
+            navMeshAgent.enabled = true;
+
+            if (destinationObject != null)
+            {
+                navMeshAgent.SetDestination(destinationObject.position);
+            }
+            else
+            {
+                Debug.LogWarning("Destination object is not set.");
+            }
+        }
     }
 }
